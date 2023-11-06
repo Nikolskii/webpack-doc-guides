2a1c4659a193ca52eccd13a08538917ee05e5e0d
Denis Nikolskii <den-apple@ya.ru>
2023-11-06 20:55:07 +0200

    Environment Variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+module.exports = (env) => {
+
+  // Use env.<YOUR VARIABLE> here:
+  console.log('Goal:', env.goal); // 'local'
+  console.log('Production', env.production); // true
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist')
+    }
+  };
 };
