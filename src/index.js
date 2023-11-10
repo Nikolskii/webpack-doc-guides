951fe6b374bebcccea5607926112d5762d3c70c8
Denis Nikolskii <den-apple@ya.ru>
2023-11-10 08:01:16 +0200

    Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import printMe from './print';
+import './styles.css';
 
 function component() {
   const element = document.createElement('div');
@@ -15,4 +16,14 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());
+let element = component(); // Store the element to re-render on print.js changes
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    document.body.removeChild(element);
+    element = component(); // Re-render the "component" to update the click handler
+    document.body.appendChild(element);
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,41 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: 'development',
   entry: {
-    index: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js',
+    // Runtime code for hot module replacement
+    hot: 'webpack/hot/dev-server.js',
+    // Dev server client for web socket transport, hot and live reload logic
+    client: 'webpack-dev-server/client/index.js?hot=true&live-reload=true'
   },
   devtool: 'inline-source-map',
   devServer: {
-    static: './dist'
+    static: './dist',
+    // Dev server client for web socket transport, hot and live reload logic
+    hot: false,
+    client: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development'
-    })
+      title: 'Hot Module Replacement'
+    }),
+    // Plugin for hot module replacement
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/'
+    clean: true
   },
   optimization: {
     runtimeChunk: 'single'
