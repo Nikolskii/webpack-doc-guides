6c75e2289914aa0d4e3a5640818eddf7c14d39c2
Denis Nikolskii <den-apple@ya.ru>
2023-11-12 18:28:16 +0200

    Production

--- webpack.common.js
+++ webpack.common.js
@@ -2,27 +2,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: {
-    index: './src/index.js',
-    print: './src/print.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist'
+    app: './src/index.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development'
+      title: 'Production'
     })
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/'
-  },
-  optimization: {
-    runtimeChunk: 'single'
+    clean: true
   }
 };
